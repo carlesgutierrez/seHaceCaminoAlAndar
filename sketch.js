78881ec184055c1e11e5727c4eb3356120d67fd0
now.havas <innovacion.now@gmail.com>
2022-04-02 17:51:27 +0200

    default params

--- sketch.js
+++ sketch.js
@@ -58,17 +58,16 @@ let sliderPosYTextInfo;
   let default_lerpSound = 0.005;
   let default_auxlerpSpeed = 0.05;
   let default_SpeedCintaCorrer = 1;// ADAPT HERE IF NEW HARDWARE ( Cinta de correr ) IS USED
-  let default_VelVideo = 6;
+  let default_VelVideo = 4;
   let default_MaxVolVideo = 0.7;
   let default_MinVolVideo = 0.1;
 
   let default_MaxVolMainAudio = 0.9;
   let default_MinVolMainAudio = 0.1;
 
-
   let default_auxtimeFadeIn = 5;
   let default_auxtimeFadeOut = 7;
-  let default_timeWithoutInteraction = 30;
+  let default_timeWithoutInteraction = 20;
 
   let default_sizeTextInfo = 30;
   let default_gapTextInfo = 5;
